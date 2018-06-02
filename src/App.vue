c26ab6b8c29b1ba68058d5f069a3e2b9d836c347
maruhachi <konchan1129@gmail.com>
2018-06-02 20:21:00 +0900

    別ページにてjQuery連携してみる

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <HelloWorld/>
+    <TableSorter/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import TableSorter from "./components/TableSorter";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    TableSorter
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/TableSorter.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h2 id="msg">haha</h2>
+    <button id="ha" @click="haha()">Ha!</button>
+    <br />
+    <table>
+      <tr>
+        <th>index</th>
+        <th>limit</th>
+        <th>metrics</th>
+        <th>usage</th>
+      </tr>
+      <tr v-for="(row, index) in rows" :key="index">
+        <td>{{index}}</td>
+        <td>{{ row.limit }}</td>
+        <td>{{ row.metric }}</td>
+        <td>{{ row.usage }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      rows: [
+        {
+          limit: 20,
+          metric: "aa",
+          usage: "0"
+        },
+        {
+          limit: 10,
+          metric: "BB",
+          usage: "99"
+        }
+      ]
+    };
+  },
+  methods: {
+    haha() {
+      $("h2").text($("h2").text() + "haha");
+    }
+  }
+};
+</script>
+<style lang="postcss" scoped>
+table {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+</style>
